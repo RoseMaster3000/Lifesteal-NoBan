afd95655580528f77af40859e96d474feaf31e45
Shahrose Kasim <rosemaster3000@gmail.com>
2025-03-23 21:40:44 -0400

    fixed max health withdraw message

--- neoforge/src/main/java/mc/mian/lifesteal/datagen/LSLangProvider.java
+++ neoforge/src/main/java/mc/mian/lifesteal/datagen/LSLangProvider.java
@@ -78,6 +78,7 @@ public class LSLangProvider extends LanguageProvider {
         addGuiMessage("heart_crystal_reaching_max", "You are already at the max amount of hearts");
         addGuiMessage("crystal_core_at_max_health", "You are already at max health");
         addGuiMessage("cant_withdraw_less_than_maximum", "You can't withdraw over the maximum amount of hearts you can lose");
+        addGuiMessage("not_full_health", "You can't withdraw unless you are fully healed.");
         addGuiMessage("weak_player_death", "You have very little HP, so you will not lose/drop any hearts :)");
         
         addGuiMessage("invaild_revive_block", "You need to right click a specified player's revive head to revive them");
